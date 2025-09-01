82dd7a444c49bf266456bfd53a16ba05c93686c7
shourov.foisal <shourov.foisal@technonext.com>
2025-09-01 14:59:12 +0600

    Fix log printing

--- main.sh
+++ main.sh
@@ -4,18 +4,18 @@ set -e
 # Download and run the bootstrap.py by using a temporary file.
 BOOTSTRAP_FILE_URL="https://raw.githubusercontent.com/shourovfoisal/python-bootstrapper/refs/heads/main/bootstrap.py"
 TEMP_FILE="$(mktemp /tmp/bootstrap.XXXXXX.py)"
-echo "Installing Dependencies..."
+printf "Installing Dependencies..."
 curl -sSL "$BOOTSTRAP_FILE_URL" -o "$TEMP_FILE"
-echo "Running the Script..."
+printf "Running the Script..."
 python3 "$TEMP_FILE"
 rm -f "$TEMP_FILE"  # Delete after use
 
 # Activate the VENV
 if [ -d "venv" ]; then
-  echo "Activating the Python Virtual Environment"
+  printf "Activating the Python Virtual Environment"
   source venv/bin/activate
-  echo "Virtual Environment Activated\nRunning the Program"
+  printf "Virtual Environment Activated\nRunning the Program..."
 else
-  echo "Could not find the VENV Directory."
+  printf "Could not find the VENV Directory."
   exit 1
 fi

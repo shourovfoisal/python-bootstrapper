0a97a41eb0be840db3dffaec469eaf5dad9fa287
shourov.foisal <shourov.foisal@technonext.com>
2025-09-01 15:08:40 +0600

    Use echo for logs

--- main.sh
+++ main.sh
@@ -4,18 +4,19 @@ set -e
 # Download and run the bootstrap.py by using a temporary file.
 BOOTSTRAP_FILE_URL="https://raw.githubusercontent.com/shourovfoisal/python-bootstrapper/refs/heads/main/bootstrap.py"
 TEMP_FILE="$(mktemp /tmp/bootstrap.XXXXXX.py)"
-printf "Installing Dependencies...\n"
+echo "Installing Dependencies..."
 curl -sSL "$BOOTSTRAP_FILE_URL" -o "$TEMP_FILE"
-printf "Running the Script...\n"
+echo "Running the Script..."
 python3 "$TEMP_FILE"
 rm -f "$TEMP_FILE"  # Delete after use
 
 # Activate the VENV
 if [ -d "venv" ]; then
-  printf "Activating the Python Virtual Environment\n"
+  echo "Activating the Python Virtual Environment"
   source venv/bin/activate
-  printf "Virtual Environment Activated\nRunning the Program...\n"
+  echo "Virtual Environment Activated"
+  echo "Running the Program..."
 else
-  printf "Could not find the VENV Directory.\n"
+  echo "Could not find the VENV Directory"
   exit 1
 fi
